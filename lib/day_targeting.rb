723985678a4822fa92556504636233f560156224
Funto <funto.afuwape@gmail.com>
2023-07-13 17:35:22 +0100

    Find weekdays of dates

--- lib/day_targeting.rb
+++ lib/day_targeting.rb
@@ -2,3 +2,18 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'date'
 require_relative 'time_targeting.rb'
+
+def weekday_finder(date)
+	Time.strptime(date, "%y/%d/%m %k:%M").wday
+end
+
+contents = CSV.open(  
+	'event_attendees.csv',
+	headers: true,
+	header_converters: :symbol
+)
+
+weekday_array = Array.new
+contents.each do |row|
+	weekday_array << weekday_finder(row[:regdate])
+end
